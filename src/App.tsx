964b6a8a472cb99e179d4096a834ad6c223e96d6
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-09 14:41:52 -0800

    add form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import { 
   fluentButton,
+  fluentCheckbox,
+  fluentTextField,
+  fluentTextArea,
+  fluentRadioGroup,
+  fluentRadio,
+  fluentSelect,
+  fluentOption,
   provideFluentDesignSystem,
 } from '@fluentui/web-components';
 import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
@@ -10,11 +17,45 @@ const { wrap } = provideReactWrapper(
   provideFluentDesignSystem()
 );
 
-export const FluentButton = wrap(fluentButton())
+const FluentButton = wrap(fluentButton()),
+      FluentCheckBox = wrap(fluentCheckbox()) as any,
+      FluentTextField = wrap(fluentTextField()),
+      FluentTextArea = wrap(fluentTextArea()),
+      FluentRadioGroup = wrap(fluentRadioGroup()) as any,
+      FluentRadio = wrap(fluentRadio()) as any,
+      FluentSelect = wrap(fluentSelect()) as any,
+      FluentOption= wrap(fluentOption()) as any
 
 
 export const App = () => {
   return (
-    <FluentButton appearance="accent">Button</FluentButton>
+    <form>
+      <h1>Example Form</h1>
+      <h5>Text Input</h5>
+      <FluentTextField placeholder="type here..."/>
+      <h5>Text Area</h5>
+      <FluentTextArea placeholder="type here..."/>
+      <h5>Checkboxes</h5>
+      <FluentCheckBox>Default</FluentCheckBox>
+      <FluentCheckBox disabled checked>Disabled</FluentCheckBox>
+      <FluentCheckBox checked>Checked</FluentCheckBox>
+      <h5>Vertical Radio Group </h5>
+      <FluentRadioGroup orientation="vertical">
+          <FluentRadio>Default</FluentRadio>
+          <FluentRadio disabled>Disabled</FluentRadio>
+      </FluentRadioGroup>
+      <FluentRadio checked>Checked</FluentRadio>
+      <h5>Dropdown selection</h5>
+      <FluentSelect title="Select your hero">
+          <FluentOption value="1">Bruce Wayne</FluentOption>
+          <FluentOption value="2">Natasha Romanoff</FluentOption>
+          <FluentOption value="3">Steven Strange</FluentOption>
+      </FluentSelect>
+      <h5>Button Group</h5>
+      <div>
+        <FluentButton appearance="accent">Primary</FluentButton>
+        <FluentButton>Secondary</FluentButton>
+      </div>
+    </form>
   );
 }
