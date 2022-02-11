7ab7bc1891de80b95226ceb13178887403a6e88a
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-11 09:39:23 -0800

    card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import './App.css';
+import {
+  provideFluentDesignSystem,
+  fluentCard
+} from "@fluentui/web-components";
+import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
+import React from 'react';
 
-import { FluentCard } from "./Card";
+const { wrap } = provideReactWrapper(
+  React, 
+  provideFluentDesignSystem()
+);
 
+const FluentCard = wrap(fluentCard());
 export const App = () => {
   return (
-    <FluentCard></FluentCard>
+    <FluentCard>
+      <div>
+        <div className="left">
+          <img src={`${process.env.PUBLIC_URL}/matt.jpg`} alt="matt avatar"/>
+        </div>
+        <div className="right">
+          <span className="title">Title goes here</span>
+          <span className="secondary"> Secondary line</span>
+        </div>
+      </div>
+      <p>Citizens of distant epochs muse about at the edge of forever hearts of the...</p>
+    </FluentCard>
   );
 }
