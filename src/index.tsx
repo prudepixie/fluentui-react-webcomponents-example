12e64ca7741c0db062768981986c6d42bd5f4305
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-23 11:13:34 -0800

    delete logs

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import React from 'react';
 
-console.log('index')
 ReactDOM.render(
   <App />,
    document.getElementById('root'),
 )
-
-
-console.log('index 2')
