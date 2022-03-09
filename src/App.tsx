83bfe426e5a373655dc0be7fbcdad295b24ea313
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-03-08 16:21:59 -0800

    add support for query params

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
-import { 
+import {
   provideFluentDesignSystem,
-  fluentButton
-} from '@fluentui/web-components';
-import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
-import React from 'react';
+  fluentButton,
+} from "@fluentui/web-components";
+import { provideReactWrapper } from "@microsoft/fast-react-wrapper";
+import React from "react";
 
-const { wrap } = provideReactWrapper(
-  React, 
-  provideFluentDesignSystem()
-);
+const { wrap } = provideReactWrapper(React, provideFluentDesignSystem());
+const FluentButton = wrap(fluentButton());
 
-const FluentButton = wrap(fluentButton())
+enum Component {
+  Button = "button",
+  Mixed = "mixed",
+  Card = "card",
+}
 
+type Repeat = string;
 
-export default () => <FluentButton appearance="accent">Button</FluentButton>
+const params = new URLSearchParams(window.location.search);
+const component = params.get("component") as Component;
+const repeat = params.get("repeatNum") as Repeat;
+
+const repeatNumber = parseInt(repeat);
+const renderComponent = () => {
+  switch (component) {
+    case Component.Button:
+      return repeatNumber > 0 ? (
+        [...Array(repeatNumber)].map((e, i) => (
+          <FluentButton key={i} appearance="accent">
+            Button
+          </FluentButton>
+        ))
+      ) : (
+        <FluentButton appearance="accent">Button</FluentButton>
+      );
+    case Component.Card:
+      return <div>this is a card</div>;
+    case Component.Mixed:
+      return <div>this is a mixed</div>;
+    default:
+      return <FluentButton appearance="accent">Button</FluentButton>;
+  }
+};
+
+export default () => <>{renderComponent()}</>;
